7c1f045af72bb6c932e1f2e16416abf78d06c958
Nodeist <104627145+Nodeist@users.noreply.github.com>
2022-05-21 01:13:44 +0300

    Create NodeistDefund.sh

---
+++ Defund/NodeistDefund.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+echo "=================================================="
+echo -e "\033[0;35m"
+echo "  /$$   /$$                 /$$           /$$             /$$    ";
+echo " | $$$ | $$                | $$          |__/            | $$    ";
+echo " | $$$$| $$  /$$$$$$   /$$$$$$$  /$$$$$$  /$$  /$$$$$$$ /$$$$$$  ";
+echo " | $$ $$ $$ /$$__  $$ /$$__  $$ /$$__  $$| $$ /$$_____/|_  $$_/  ";
+echo " | $$  $$$$| $$  \ $$| $$  | $$| $$$$$$$$| $$|  $$$$$$   | $$    ";
+echo " | $$\  $$$| $$  | $$| $$  | $$| $$_____/| $$ \____  $$  | $$ /$$ ";
+echo " | $$ \  $$|  $$$$$$/|  $$$$$$$|  $$$$$$$| $$ /$$$$$$$/  |  $$$$/ ";
+echo " |__/  \__/ \______/  \_______/ \_______/|__/|_______/    \___/  ";
+echo -e "\e[0m"
+echo "=================================================="
+                                                              
+
+sleep 2
+
+# set vars
+if [ ! $NODENAME ]; then
+	read -p "Enter node name: " NODENAME
+	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+fi
+echo "export WALLET=wallet" >> $HOME/.bash_profile
+echo "export CHAIN_ID=defund-private-1" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+echo '================================================='
+echo 'Your node name: ' $NODENAME
+echo 'Your wallet name: ' $WALLET
+echo 'Your chain name: ' $CHAIN_ID
+echo '================================================='
+sleep 2
+
+echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
+# update
+sudo apt update && sudo apt upgrade -y
+
+echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
+# packages
+sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y
+
+# install go
+ver="1.17.2"
+cd $HOME
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+source ~/.bash_profile
+go version
+
+echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
+# download binary
+cd $HOME
+git clone https://github.com/defund-labs/defund
+cd defund
+make install
+
+# config
+defundd config chain-id $CHAIN_ID
+defundd config keyring-backend file
+
+# init
+defundd init $NODENAME --chain-id $CHAIN_ID
+
+# download addrbook and genesis
+wget -qO $HOME/.defund/config/genesis.json "https://raw.githubusercontent.com/defund-labs/defund/163e2669b6870aa26b73d843312b22c9948b29c6/testnet/private/genesis.json"
+wget -qO $HOME/.defund/config/addrbook.json "https://raw.githubusercontent.com/kj89/testnet_manuals/main/defund/addrbook.json"
+
+# set minimum gas price
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0ufetf\"/" $HOME/.defund/config/app.toml
+
+# set peers and seeds
+SEEDS="8e1590558d8fede2f8c9405b7ef550ff455ce842@51.79.30.9:26656,bfffaf3b2c38292bd0aa2a3efe59f210f49b5793@51.91.208.71:26656,106c6974096ca8224f20a85396155979dbd2fb09@198.244.141.176:26656"
+PEERS="79a1ca999264d15653e359742ecc696ff783e057@49.12.246.112:22256,5b10a67cad723fd13060761f8955f371fb1810a2@80.64.208.121:26656,cd3a17a6920bba732f1b2a4b3a12a435ac0845ac@49.12.225.248:26656,0409ad6d8ceef8ab01f4df458dbd58dd9ac32295@121.37.242.170:26656,6c80295b4c221e19cab7dfab496e9c15891f55ba@65.108.151.86:26656,2ef9373a0e8b5487b6fbf100d90faa641242899d@154.12.244.137:26656,1bf56637dcb950453c370ef7726da74436d21a61@95.214.52.200:26656,b9acccdd67617e15c361ea0d6fd2e16c1b9c9efc@209.145.48.178:26656,111ba4e5ae97d5f294294ea6ca03c17506465ec5@208.68.39.221:26656,0409ad6d8ceef8ab01f4df458dbd58dd9ac32295@121.37.242.170:26656"
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.defund/config/config.toml
+
+# enable prometheus
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.defund/config/config.toml
+
+# add external (if dont use sentry), port is default
+# external_address=$(wget -qO- eth0.me)
+# sed -i -e "s/^external_address = \"\"/external_address = \"$external_address:26656\"/" $HOME/.defund/config/config.toml
+
+# config pruning
+pruning="custom"
+pruning_keep_recent="100"
+pruning_keep_every="0"
+pruning_interval="10"
+
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.defund/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.defund/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.defund/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.defund/config/app.toml
+
+# reset
+defundd tendermint unsafe-reset-all
+
+echo -e "\e[1m\e[32m4. Starting service... \e[0m" && sleep 1
+# create service
+tee $HOME/defundd.service > /dev/null <<EOF
+[Unit]
+Description=defundd
+After=network.target
+[Service]
+Type=simple
+User=$USER
+ExecStart=$(which defundd) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo mv $HOME/defundd.service /etc/systemd/system/
+
+# start service
+sudo systemctl daemon-reload
+sudo systemctl enable defundd
+sudo systemctl restart defundd
+
+echo '=============== SETUP FINISHED ==================='
+echo -e 'To check logs: \e[1m\e[32mjournalctl -ujournalctl -u defundd -f -o cat\e[0m'
+echo -e 'To check sync status: \e[1m\e[32mcurl -s localhost:26657/status | jq .result.sync_info\e[0m'
