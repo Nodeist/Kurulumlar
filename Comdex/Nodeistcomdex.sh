7ce6c954763205fd269d22be513937e2450676e6
Nodeist <104627145+Nodeist@users.noreply.github.com>
2022-05-29 22:32:01 +0300

    Update Nodeistcomdex.sh

--- Comdex/Nodeistcomdex.sh
+++ Comdex/Nodeistcomdex.sh
@@ -96,6 +96,20 @@ sed -i.bak -e "s/^external_address *=.*/external_address = \"$external_address:3
 
 sleep 1 
 
+SNAP_RPC="https://comdex-testnet-rpc.polkachu.com:443"
+
+LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000)); \
+TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+
+sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
+s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
+s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
+s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"\"|" $HOME/.comdex/config/config.toml
+
+sleep 1
+
 # reset
 comdex tendermint unsafe-reset-all
 
