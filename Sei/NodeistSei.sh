02470bc62f102b508aca6dbdedaf0ab475b24a11
Nodeist <104627145+Nodeist@users.noreply.github.com>
2022-06-02 02:25:07 +0300

    Update NodeistSei.sh

--- Sei/NodeistSei.sh
+++ Sei/NodeistSei.sh
@@ -49,9 +49,9 @@ go version
 echo -e "\e[1m\e[32m3. kutuphaneler indirilip yukleniyor... \e[0m" && sleep 1
 # download binary
 cd $HOME
-git clone --depth 1 --branch 1.0.1beta https://github.com/sei-protocol/sei-chain.git
-cd sei
-git checkout 1.0.1beta
+git clone https://github.com/sei-protocol/sei-chain.git
+cd sei-chain
+git checkout 1.0.0beta
 go build -o build/seid ./cmd/sei-chaind
 chmod +x ./build/seid && sudo mv ./build/seid /usr/local/bin/seid
 
@@ -63,19 +63,19 @@ seid config keyring-backend file
 seid init $NODENAME --chain-id $CHAIN_ID
 
 # download genesis and addrbook
-wget -qO $HOME/.sei/config/genesis.json "https://raw.githubusercontent.com/sei-protocol/testnet/master/sei-testnet-1/genesis.json"
-wget -qO $HOME/.sei/config/addrbook.json "https://raw.githubusercontent.com/sei-protocol/testnet/master/sei-testnet-1/addrbook.json"
+wget -qO $HOME/.sei-chain/config/genesis.json "https://raw.githubusercontent.com/sei-protocol/testnet/master/sei-testnet-1/genesis.json"
+wget -qO $HOME/.sei-chain/config/addrbook.json "https://raw.githubusercontent.com/sei-protocol/testnet/master/sei-testnet-1/addrbook.json"
 
 # set minimum gas price
-sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0usei\"/" $HOME/.sei/config/app.toml
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0usei\"/" $HOME/.sei-chain/config/app.toml
 
 # set peers and seeds
 SEEDS=""
-PEERS="b7972b95366cb451f4e3004b6cc3a62f656d81e4@195.2.70.182:26656,a36ee0a2aa73e32910300d5ed2b6e2c0b76861aa@144.91.92.129:26656,7c44dd10010a67f2874314e23a5885c0a067e2e1@62.113.117.51:26656,7f192ef8b05ece5acfd21fcbf4e1288d91265243@195.3.221.48:27656,6840ba2f975da5d1ff02573d7bc9ddfdd7134965@95.216.75.106:11656,9ed9f8d2e3a7f442c234f78d79c2c0e9916ed315@144.76.91.26:26656,352ddd90bbfc7e4f1a98504cbee293108b99673c@85.14.247.139:26656,16b29852507b09f8264f54b8b1e6aa2a0f263a5d@65.108.222.181:26656,724b2377b8f0de7533b71fdb3413d31d16a6aed7@213.136.70.209:26656,bbf3fb6602fc77cda647444cd198d0514acb3623@164.68.120.106:26656,230ebb6472866cf5254c88433f3f4849ecdfd38b@168.119.181.213:26656,c989ecf4521ad739b21179e949d88e3f6fb9233f@176.57.150.149:26641,3de20b80b0230642cb3415a18ba22642e5456523@65.108.138.183:8095,6a78a77c7427c5e4377f0055be67077ee769021a@147.182.158.179:26656,361423bbea64f125d500024dce989b4fe8bc2a52@185.239.209.209:26656,2ed66e6f4d146860085d00d00d748be5ca44809d@46.101.187.13:26656,aff15f34d1e7959619638b338a5fa792be14c415@5.189.145.154:26656,e8841d62fa0fffad13e0578b438a7fe020bc3588@65.108.75.148:26656,1088d4a40eefe813814025ba81a45be21bc9faa0@167.86.72.114:26656,0f8c2f0a4fcd5ed0fca69979dcbbd59e3de64eae@167.235.253.41:26656,688f78994e63665ecd426571d0d56c8e5310d81f@89.163.223.85:26656,5023d370719e0d6e5da0ac27ad89c0c5646584df@167.86.117.19:26656,85917f092c13a8d3d58d136dd72888d5a06fc332@66.94.101.212:26656,b1f397bee3b918f9bbbe23614c7f285f2596a1e1@135.181.136.33:16656,b19bc0551fdc538b85577f6785eb52f76e0ba745@195.2.84.133:26656,6a876e2c9f78a6b086e30f9bcdbdb3b964e50a0f@65.21.132.27:44656,c2a17094321b208ecef4057877b2df11aff0b672@161.97.181.63:26656,dbe05528e8fd087306d05a149462ea22082abb8c@167.235.228.221:26656,75a86059c4727c89db8cb6d75dcf6ee7a11ff665@109.205.180.116:26656,03444777ba8d4ecb45cb0aecb5dde60cb8600da0@65.109.1.107:26656,3590457f1396f2a9a26537eddcaea50bec69342b@65.108.193.210:7095,5655dce3b5802231780755b535555cf960d5d2f9@95.217.118.100:30656,d62154f44941a740ef29cd27f031fed3fd63ff42@168.119.183.189:26656,81a00b254ebdd07f207d3dd2c989ef9efcf9c04c@165.232.128.14:26656,49e9d66477cd5df48ceb884b6870cccfc5fa96c5@47.156.153.124:56656,dc882e58c0c51763a12423dfcac5815ef092bc29@65.108.202.114:26656,23b194ac659ed04bd1fd7433ef025195dc6ce8c2@194.163.179.176:36656,941d775a2f6891db842db4f6142d25957364ccd5@195.201.224.0:26656,7c961e45dbd97c6ffa4869002e8381e1a7617c7e@66.94.117.205:26656,84838a89b697a74475d3e1d015b3b3176ec327e2@185.252.234.103:26656,f9b6f0e2ab372942a4a292a40f26799e219a2cb2@65.108.208.175:26656,b6f64367a7049c6333b0d5b9725bf98881b4288b@178.128.225.238:26656,80db075c4cd4bc8a0572680efdd7f3f62c70bc47@185.215.165.213:26656,220a28ca16eb52b37ef4f7b569e712d895e38ceb@65.21.89.42:22656,b7c64f6d33f97ed357414e83fb73ff1c738d85e4@185.144.99.65:26656,fee894a7b11b76194093fea75a12fd69e493eb54@167.235.228.147:26651,b4bd73ff6bab715ca1d6bda93f3c5ff126390ff5@185.211.6.53:26656,2a1f004695968cb4c620abb8e2d0cc8590afd727@194.163.136.65:26656,5d99b92df276ccbb609cc16261d2cc7da0a003a9@45.77.45.139:26656"
-sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.sei/config/config.toml
+PEERS="585727dac5df8f8662a8ff42052a9584a1f7ee95@165.22.25.77:26656,2f047e234cb8b99fe8b9fee0059a5bc45042bc97@95.216.84.188:26656,bab4849cf3918c37b04cd3714984d1765616a4b2@49.12.76.255:36656,ab955dc7f70d8613ab4b554868d4658fd70b797b@217.79.180.194:26656,39c4bcaded0d1d886f2788ae955f1939406f3e7d@65.108.198.54:26696,9db58dba3b6354177fb428caccf5167c616ad4a1@167.235.28.18:26656,2f2804434afda302c86eb89eca27503e49a8a260@65.21.131.215:26696,38b4d78c7d6582fb170f6c19330a7e37e6964212@194.163.189.114:46656,7c5ee0d66a15013f0a771055378c5316331f17ba@95.216.101.84:25646,6f71bcbe347069fc4df9b607f6b843226e8deb71@95.217.221.201:26656"
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.sei-chain/config/config.toml
 
 # enable prometheus
-sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.sei/config/config.toml
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.sei-chain/config/config.toml
 
 # config pruning
 pruning="custom"
@@ -84,9 +84,9 @@ pruning_keep_every="0"
 pruning_interval="10"
 
 sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.sei-chain/config/app.toml
-sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.sei/config/app.toml
-sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.sei/config/app.toml
-sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.sei/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.sei-chain/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.sei-chain/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.sei-chain/config/app.toml
 
 sleep 1
 
