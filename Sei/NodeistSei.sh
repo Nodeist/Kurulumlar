575dd39aa8b7fc0c8da981ef41e6d64a167d750f
Nodeist <104627145+Nodeist@users.noreply.github.com>
2022-06-02 02:04:29 +0300

    Update NodeistSei.sh

--- Sei/NodeistSei.sh
+++ Sei/NodeistSei.sh
@@ -50,10 +50,7 @@ echo -e "\e[1m\e[32m3. kutuphaneler indirilip yukleniyor... \e[0m" && sleep 1
 # download binary
 cd $HOME
 git clone --depth 1 --branch 1.0.1beta https://github.com/sei-protocol/sei-chain.git
-cd sei-chain
-git checkout 1.0.1beta
-go build -o build/seid ./cmd/sei-chaind
-chmod +x ./build/seid && sudo mv ./build/seid /usr/local/bin/seid
+make install
 
 # config
 seid config chain-id $CHAIN_ID
@@ -99,17 +96,6 @@ sed -i.bak -e "s/^external_address *=.*/external_address = \"$external_address:3
 
 sleep 1 
 
-SNAP_RPC="https://sei-testnet-rpc.polkachu.com:443"
-
-LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
-BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000)); \
-TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
-
-sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
-s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
-s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
-s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
-s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"\"|" $HOME/.sei/config/config.toml
 
 # reset
 seid unsafe-reset-all
