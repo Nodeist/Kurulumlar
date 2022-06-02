7abe46bfaa73e3ec1c9cbc9cc50be62c4cfe22d6
Nodeist <104627145+Nodeist@users.noreply.github.com>
2022-06-02 04:56:18 +0300

    Update NodeistSei.sh

--- Sei/NodeistSei.sh
+++ Sei/NodeistSei.sh
@@ -51,7 +51,7 @@ echo -e "\e[1m\e[32m3. kutuphaneler indirilip yukleniyor... \e[0m" && sleep 1
 cd $HOME
 git clone https://github.com/sei-protocol/sei-chain.git
 cd sei-chain
-git checkout 1.0.1 beta
+git checkout 1.0.1beta
 go build -o build/seid ./cmd/sei-chaind
 chmod +x ./build/seid && sudo mv ./build/seid /usr/local/bin/seid
 mv $HOME/go/bin/seid /usr/local/bin/
