a36987ce3a1ef82e7293c0af90414c38bdfc60f9
Nodeist <104627145+Nodeist@users.noreply.github.com>
2022-06-20 21:15:50 +0300

    Update Nodeistpaloma.sh

--- Paloma/Nodeistpaloma.sh
+++ Paloma/Nodeistpaloma.sh
@@ -58,8 +58,8 @@ palomad config keyring-backend test
 palomad init $NODENAME --chain-id $CHAIN_ID
 
 # download genesis and addrbook
-wget -qO ~/.paloma/config/genesis.json https://raw.githubusercontent.com/palomachain/testnet/master/iona/genesis.json
-wget -qO ~/.paloma/config/addrbook.json https://raw.githubusercontent.com/palomachain/testnet/master/iona/addrbook.json
+wget -qO ~/.paloma/config/genesis.json https://raw.githubusercontent.com/palomachain/testnet/master/passerina/genesis.json
+wget -qO ~/.paloma/config/addrbook.json https://raw.githubusercontent.com/palomachain/testnet/master/passerina/addrbook.json
 
 # set minimum gas price
 sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0grain\"/" $HOME/.paloma/config/app.toml
