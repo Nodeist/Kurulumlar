67c448221303cae258feac835bfdc7d8c7b947c8
Nodeist <104627145+Nodeist@users.noreply.github.com>
2022-06-22 05:28:12 +0300

    Update Nodeistseigentx.sh

--- Sei/Gentx/Nodeistseigentx.sh
+++ Sei/Gentx/Nodeistseigentx.sh
@@ -17,7 +17,7 @@ if [ ! $NODENAME ]; then
 	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
 fi
 echo "export WALLET=wallet" >> $HOME/.bash_profile
-echo "export CHAIN_ID=sei-testnet-2" >> $HOME/.bash_profile
+echo "export CHAIN_ID=sei-testnet-3" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
 echo '================================================='
