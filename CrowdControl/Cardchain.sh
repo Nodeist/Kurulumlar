052eb715aac821d542778527ec066a2bc2598138
Nodeist <104627145+Nodeist@users.noreply.github.com>
2022-07-12 03:43:23 +0300

    Update Cardchain.sh

--- CrowdControl/Cardchain.sh
+++ CrowdControl/Cardchain.sh
@@ -84,10 +84,10 @@ sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $
 
 sleep 1
 
-#Change port 36
-sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:36368\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:36367\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:6361\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:36366\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":36360\"%" $HOME/.Cardchain/config/config.toml
-sed -i.bak -e "s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:9360\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:9361\"%" $HOME/.Cardchain/config/app.toml
-sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:36367\"%" $HOME/.Cardchain/config/client.toml
+#Change port 43
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:36438\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:36437\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:6431\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:36436\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":36430\"%" $HOME/.Cardchain/config/config.toml
+sed -i.bak -e "s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:9430\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:9431\"%" $HOME/.Cardchain/config/app.toml
+sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:36437\"%" $HOME/.Cardchain/config/client.toml
 external_address=$(wget -qO- eth0.me)
 sed -i.bak -e "s/^external_address *=.*/external_address = \"$external_address:36366\"/" $HOME/.Cardchain/config/config.toml
 
