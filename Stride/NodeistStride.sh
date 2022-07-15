ed6ef922c1b2525a895918e73a49aa1cc59b6ea9
Nodeist <104627145+Nodeist@users.noreply.github.com>
2022-07-15 08:48:49 +0300

    Update NodeistStride.sh

--- Stride/NodeistStride.sh
+++ Stride/NodeistStride.sh
@@ -1,138 +1,170 @@
 #!/bin/bash
 echo "=================================================="
-echo -e "\033[0;35m"
-echo " | \ | |         | |    (_)   | |  ";
-echo " |  \| | ___   __| | ___ _ ___| |_ ";
-echo " |     |/ _ \ / _  |/ _ \ / __| __| ";
-echo " | |\  | (_) | (_| |  __/ \__ \ |_ ";
-echo " |_| \_|\___/ \__,_|\___|_|___/\__| ";
+echo "   _  ______  ___  __________________";
+echo "  / |/ / __ \/ _ \/ __/  _/ __/_  __/";
+echo " /    / /_/ / // / _/_/ /_\ \  / /   ";
+echo "/_/|_/\____/____/___/___/___/ /_/    ";
 echo -e "\e[0m"
-echo "=================================================="  
+echo "=================================================="                                     
+
 
 sleep 2
 
-# set vars
-if [ ! $NODENAME ]; then
-	read -p "Node ismi yaziniz: " NODENAME
-	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
-fi
-echo "export WALLET=wallet" >> $HOME/.bash_profile
-echo "export CHAIN_ID=STRIDE-1" >> $HOME/.bash_profile
+# DEGISKENLER by Nodeist
+STRD_WALLET=wallet
+STRD=strided
+STRD_ID=STRIDE-1
+STRD_PORT=44
+STRD_FOLDER=.stride
+STRD_FOLDER2=stride
+STRD_VER=c53f6c562d9d3e098aab5c27303f41ee055572cb
+STRD_REPO=https://github.com/Stride-Labs/stride.git
+STRD_GENESIS=https://raw.githubusercontent.com/Stride-Labs/testnet/main/poolparty/genesis.json
+STRD_ADDRBOOK=
+STRD_MIN_GAS=0
+STRD_DENOM=ustrd
+STRD_SEEDS=baee9ccc2496c2e3bebd54d369c3b788f9473be9@seedv1.poolparty.stridenet.co:26656
+STRD_PEERS=b11187784240586475422b132a3dcbc970a996dd@stride-node1.poolparty.stridenet.co:26656,2312417b613c44164bf167cb232795e7d8815be7@65.108.76.44:11523,84ff28824a911409e2c24f2f5ede87ae1b859b5f@5.189.178.222:46656
+
+sleep 1
+
+echo "export STRD_WALLET=${STRD_WALLET}" >> $HOME/.bash_profile
+echo "export STRD=${STRD}" >> $HOME/.bash_profile
+echo "export STRD_ID=${STRD_ID}" >> $HOME/.bash_profile
+echo "export STRD_PORT=${STRD_PORT}" >> $HOME/.bash_profile
+echo "export STRD_FOLDER=${STRD_FOLDER}" >> $HOME/.bash_profile
+echo "export STRD_FOLDER2=${STRD_FOLDER2}" >> $HOME/.bash_profile
+echo "export STRD_VER=${STRD_VER}" >> $HOME/.bash_profile
+echo "export STRD_REPO=${STRD_REPO}" >> $HOME/.bash_profile
+echo "export STRD_GENESIS=${STRD_GENESIS}" >> $HOME/.bash_profile
+echo "export STRD_PEERS=${STRD_PEERS}" >> $HOME/.bash_profile
+echo "export STRD_SEED=${STRD_SEED}" >> $HOME/.bash_profile
+echo "export STRD_MIN_GAS=${STRD_MIN_GAS}" >> $HOME/.bash_profile
+echo "export STRD_MIN_GAS=${STRD_DENOM}" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
+sleep 1
+
+if [ ! $STRD_NODENAME ]; then
+  read -p "NODE ISMI YAZINIZ: " STRD_NODENAME
+  echo 'export STRD_NODENAME='$STRD_NODENAME >> $HOME/.bash_profile
+fi
+
+echo -e "NODE ISMINIZ: \e[1m\e[32m$STRD_NODENAME\e[0m"
+echo -e "CUZDAN ISMINIZ: \e[1m\e[32m$STRD_WALLET\e[0m"
+echo -e "CHAIN ISMI: \e[1m\e[32m$STRD_ID\e[0m"
+echo -e "PORT NUMARANIZ: \e[1m\e[32m$STRD_PORT\e[0m"
 echo '================================================='
-echo 'Node isminiz: ' $NODENAME
-echo 'Cüzdan isminiz: ' $WALLET
-echo 'Chain ismi: ' $CHAIN_ID
-echo '================================================='
+
 sleep 2
 
-echo -e "\e[1m\e[32m1. Paketler güncelleniyor... \e[0m" && sleep 1
-# update
+
+# GUNCELLEMELER by Nodeist
+echo -e "\e[1m\e[32m1. GUNCELLEMELER YUKLENIYOR... \e[0m" && sleep 1
 sudo apt update && sudo apt upgrade -y
 
-echo -e "\e[1m\e[32m2. Bagliliklar yukleniyor... \e[0m" && sleep 1
-# packages
+
+# GEREKLI PAKETLER by Nodeist
+echo -e "\e[1m\e[32m2. GEREKLILIKLER YUKLENIYOR... \e[0m" && sleep 1
 sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y
 
-# install go
-ver="1.18.1" && \
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
-sudo rm -rf /usr/local/go && \
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
-rm "go$ver.linux-amd64.tar.gz" && \
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile && \
-source $HOME/.bash_profile && \
+# GO KURULUMU by Nodeist
+echo -e "\e[1m\e[32m1. GO KURULUYOR... \e[0m" && sleep 1
+ver="1.18.2"
+cd $HOME
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+source ~/.bash_profile
 go version
 
+sleep 1
 
-echo -e "\e[1m\e[32m3. kutuphaneler indirilip yukleniyor... \e[0m" && sleep 1
-# download binary
-git clone https://github.com/Stride-Labs/stride.git && cd stride
-git checkout c53f6c562d9d3e098aab5c27303f41ee055572cb
+# KUTUPHANE KURULUMU by Nodeist
+echo -e "\e[1m\e[32m1. REPO YUKLENIYOR... \e[0m" && sleep 1
+cd $HOME
+git clone $STRD_REPO
+cd $STRD_FOLDER2
+git checkout $STRD_VER
 sh ./scripts-local/build.sh -s $HOME/go/bin
 
 
-# config
-strided config chain-id $CHAIN_ID
-strided config keyring-backend test
+sleep 1
 
-# init
-strided init $NODENAME --chain-id $CHAIN_ID
+# KONFIGURASYON by Nodeist
+echo -e "\e[1m\e[32m1. KONFIGURASYONLAR AYARLANIYOR... \e[0m" && sleep 1
+$STRD config chain-id $STRD_ID
+$STRD config keyring-backend file
+$STRD init $STRD_NODENAME --chain-id $STRD_ID
 
-# download genesis
-wget -O $HOME/.stride/config/genesis.json "https://raw.githubusercontent.com/Stride-Labs/testnet/main/poolparty/genesis.json"
+# ADDRBOOK ve GENESIS by Nodeist
+wget $STRD_GENESIS -O $HOME/$STRD_FOLDER/config/genesis.json
+wget $STRD_ADDRBOOK -O $HOME/$STRD_FOLDER/config/addrbook.json
 
-# set minimum gas price
-sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.0025ustrd\"/;" ~/.stride/config/app.toml
+# EŞLER VE TOHUMLAR by Nodeist
+SEEDS="$STRD_SEEDS"
+PEERS="$STRD_PEERS"
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/$STRD_FOLDER/config/config.toml
 
-# set peers and seeds
-SEEDS="baee9ccc2496c2e3bebd54d369c3b788f9473be9@seedv1.poolparty.stridenet.co:26656"
-PEERS="b11187784240586475422b132a3dcbc970a996dd@stride-node1.poolparty.stridenet.co:26656,2312417b613c44164bf167cb232795e7d8815be7@65.108.76.44:11523,84ff28824a911409e2c24f2f5ede87ae1b859b5f@5.189.178.222:46656"
+sleep 1
 
-sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.stride/config/config.toml
-
-# enable prometheus
-sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.stride/config/config.toml
-
-#Set max peers
-sed -i 's/max_num_inbound_peers =.*/max_num_inbound_peers = 100/g' $HOME/.stride/config/config.toml
-sed -i 's/max_num_outbound_peers =.*/max_num_outbound_peers = 100/g' $HOME/.stride/config/config.toml
 
 # config pruning
 pruning="custom"
 pruning_keep_recent="100"
 pruning_keep_every="0"
-pruning_interval="10"
-
-sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.stride/config/app.toml
-sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.stride/config/app.toml
-sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.stride/config/app.toml
-sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.stride/config/app.toml
-
-sleep 1
-
-#Change port 44
-sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:36448\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:36447\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:6441\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:36446\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":36440\"%" $HOME/.stride/config/config.toml
-sed -i.bak -e "s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:9440\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:9441\"%" $HOME/.stride/config/app.toml
-sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:36447\"%" $HOME/.stride/config/client.toml
-external_address=$(wget -qO- eth0.me)
-sed -i.bak -e "s/^external_address *=.*/external_address = \"$external_address:36446\"/" $HOME/.stride/config/config.toml
-
-sleep 1 
-
-#Set Indexer null
-indexer="null"
-sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $HOME/.stride/config/config.toml
+pruning_interval="50"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/$STRD_FOLDER/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/$STRD_FOLDER/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/$STRD_FOLDER/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/$STRD_FOLDER/config/app.toml
 
 
-# reset
-strided tendermint unsafe-reset-all --home $HOME/stride
+# ÖZELLEŞTİRİLMİŞ PORTLAR by Nodeist
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${STRD_PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${STRD_PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${STRD_PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${STRD_PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${STRD_PORT}660\"%" $HOME/$STRD_FOLDER/config/config.toml
+sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${STRD_PORT}317\"%; s%^address = \":8080\"%address = \":${STRD_PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${STRD_PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${STRD_PORT}091\"%" $HOME/$STRD_FOLDER/config/app.toml
+sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:${STRD_PORT}657\"%" $HOME/$STRD_FOLDER/config/client.toml
 
-echo -e "\e[1m\e[32m4. Servisler baslatiliyor... \e[0m" && sleep 1
+# PROMETHEUS AKTIVASYON by Nodeist
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/$STRD_FOLDER/config/config.toml
+
+# MINIMUM GAS AYARI by Nodeist
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.00125$STRD_DENOM\"/" $HOME/$STRD_FOLDER/config/app.toml
+
+# INDEXER AYARI by Nodeist
+indexer="null" && \
+sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $HOME/$STRD_FOLDER/config/config.toml
+
+# RESET by Nodeist
+$STRD tendermint unsafe-reset-all --home $HOME/$STRD_FOLDER
+
+echo -e "\e[1m\e[32m4. SERVIS BASLATILIYOR... \e[0m" && sleep 1
 # create service
-tee $HOME/strided.service > /dev/null <<EOF
+sudo tee /etc/systemd/system/$STRD.service > /dev/null <<EOF
 [Unit]
-Description=strided
+Description=$STRD
 After=network.target
 [Service]
 Type=simple
 User=$USER
-ExecStart=$(which strided) start
-Restart=always
-RestartSec=3
-LimitNOFILE=infinity
+ExecStart=$(which $STRD) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
 [Install]
 WantedBy=multi-user.target
 EOF
 
-sudo mv $HOME/strided.service /etc/systemd/system/
 
-# start service
+# SERVISLERI BASLAT by Nodeist
 sudo systemctl daemon-reload
-sudo systemctl enable strided
-sudo systemctl restart strided
+sudo systemctl enable $STRD
+sudo systemctl restart $STRD
 
-echo '=============== KURULUM BASARIYLA TAMAMLANDI ==================='
-echo -e 'Loglari kontrol et: \e[1m\e[32mjournalctl -fu strided -o cat\e[0m'
-echo -e 'Senkronizasyon durumu kontrol et: \e[1m\e[32mcurl -s localhost:26657/status | jq .result.sync_info\e[0m'
+echo '=============== KURULUM TAMAM! by Nodeist ==================='
+echo -e 'LOGLARI KONTROL ET: \e[1m\e[32mjournalctl -f $STRD\e[0m'
+echo -e "SENKRONIZASYONU KONTROL ET: \e[1m\e[32mcurl -s localhost:${STRD_PORT}657/status | jq .result.sync_info\e[0m"
+
+source $HOME/.bash_profile
