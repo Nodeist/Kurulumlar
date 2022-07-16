98fcf73a8f349c2293a2f42d87fbb26f03067a50
Nodeist <104627145+Nodeist@users.noreply.github.com>
2022-07-16 22:45:11 +0300

    Add files via upload

---
+++ Celestia/Tools/Autostake.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo "=================================================="
+echo "   _  ______  ___  __________________";
+echo "  / |/ / __ \/ _ \/ __/  _/ __/_  __/";
+echo " /    / /_/ / // / _/_/ /_\ \  / /   ";
+echo "/_/|_/\____/____/___/___/___/ /_/    ";
+echo -e "\e[0m"
+echo "=================================================="                                     
+
+
+sleep 2
+
+############ SET PROPERTIES #########
+
+ADDRESS="walletadress"
+VALIDATOR="valoperadress"
+KEY_NAME="walletname"
+PASS="walletpass"
+CHAIN_ID="chainid"
+GAS_VALUE="auto"
+#FEE_VALUE=""
+
+############ AUTO DELEGATION #########
+
+# Withdraw
+while :
+do
+	echo $PASS | celestia-appd tx distribution withdraw-rewards "${VALIDATOR}"  --from "${KEY_NAME}" --commission --chain-id=${CHAIN_ID} --gas="${GAS_VALUE}" -y
+	
+	sleep 20s
+	
+	AVAILABLE_COIN=$(celestia-appd query bank balances ${ADDRESS} --output json | jq -r '.balances | map(select(.denom == "utia")) | .[].amount' | tr -cd [:digit:])
+	KEEP_FOR_FEES=100000
+	AMOUNT=$(($AVAILABLE_COIN - $KEEP_FOR_FEES))
+	AMOUNT_FINAL=$AMOUNT"utia"
+	
+	
+	# Delegate
+	echo $PASS | celestia-appd tx staking delegate "${VALIDATOR}" "${AMOUNT_FINAL}" --from "${KEY_NAME}" --chain-id=${CHAIN_ID} -y
+	date
+	sleep 90s
+done;
