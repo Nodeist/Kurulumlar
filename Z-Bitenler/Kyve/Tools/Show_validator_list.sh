1307504fad15e8870132d1980cb8cc054bc1fb55
Nodeist <104627145+Nodeist@users.noreply.github.com>
2022-07-16 23:12:09 +0300

    Add files via upload

---
+++ Z-Bitenler/Kyve/Tools/Show_validator_list.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "=================================================="
+echo "   _  ______  ___  __________________";
+echo "  / |/ / __ \/ _ \/ __/  _/ __/_  __/";
+echo " /    / /_/ / // / _/_/ /_\ \  / /   ";
+echo "/_/|_/\____/____/___/___/___/ /_/    ";
+echo -e "\e[0m"
+echo "=================================================="                                     
+
+
+sleep 2
+
+set -e
+
+kyved query staking validators -o json | \
+jq .validators[] | \
+jq -s 'sort_by(.tokens) | reverse' | \
+jq -r '["Validator", "VP"], ["----------------", "------------"], (.[] | [.description.moniker, (.tokens|tonumber/1000000)]) | @tsv' | \
+column -t -s "$(printf '\t')"
